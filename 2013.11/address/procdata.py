52a87bebe3b8a46d292d54c7ec4fa38278f9d2a1
liugehao <root@workgroup.cn>
2013-11-11 15:11:35 +0800

    up

--- 2013.11/address/procdata.py
+++ 2013.11/address/procdata.py
@@ -4,6 +4,8 @@
 import MySQLdb, psycopg2
 import sys
 import re
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 pconn = psycopg2.connect(host='172.16.147.133', user='postgres', password='l', database='address')
 pconn.set_client_encoding('utf-8')
@@ -11,22 +13,35 @@ pcw = pconn.cursor()
 pc = pconn.cursor()
 
 def procs(strtmp, a, b=None):
-	for x in a:
-		if strtmp.find(x) == 0:
-			return re.sub(r'^%s' % x, '', strtmp)
-	if b is not None:
-		for x in b:
-			if strtmp.find(x) == 0:
-				return re.sub(r'^%s' % x, '', strtmp)
-	return strtmp
+    for x in a:
+        if strtmp.find(x) == 0:
+            return re.sub(r'^%s' % x, '', strtmp)
+    if b is not None:
+        for x in b:
+            if strtmp.find(x) == 0:
+                return re.sub(r'^%s' % x, '', strtmp)
+    return strtmp
 
 def proc(row):
-	tmp = procs(row[1], prov1, prov2)
-	tmp = procs(tmp, city1)
-	tmp = procs(tmp, ctry )
-	
-	print row[1],'|', tmp
-	return tmp
+    tmp = procs(row[1].decode('utf-8'), prov1, prov2)
+    tmp = procs(tmp, city1)
+    tmp = procs(tmp, city2)
+    tmp = procs(tmp, ctry )
+    tmp = procsa(tmp)
+    
+    print row[1],'|', tmp
+    return tmp
+
+res = {}
+res['num']= re.compile('\d*?-*?\d*?-\d*.*$')
+res['num1'] = re.compile(u'\d*?-*?\d*?[号栋幢室].*$')
+res['rep'] = re.compile(u'(大[楼厦院]|公司|学校|分校|宿舍|[a-zA-Z0-9]{1,3}座).*$')
+
+def procsa(tmp):
+    tmp = res['num'].sub('',tmp)
+    tmp = res['num1'].sub('',tmp)
+    tmp = res['rep'].sub(lambda x:x.group(1), tmp)
+    return tmp
 
 #pc.execute("""CREATE TABLE ldb2
 #(
@@ -38,38 +53,41 @@ def proc(row):
 #pconn.commit()
 
 pc.execute("""select prov_coding,
-		prov_name
-	from prov""")
+        prov_name
+    from prov""")
 
 prov1 = []
 prov2 = []
 for row in pc.fetchall():
-	prov1.append(row[1])
-	try:
-		prov2.append(row[1].replace('省','').replace('市','').replace('自治区',''))
-	except:
-		print 'error:1:', row[1]
+    prov1.append(row[1])
+    try:
+        prov2.append(row[1].replace(u'省','').replace(u'市','').replace(u'自治区',''))
+    except:
+        print 'error:1:', row[1]
 
 pc.execute("""select city_name
-	from city""")
+    from city""")
 city1 = []
+city2 = []
 for row in pc.fetchall():
-	city1.append(row[0])
+    city1.append(row[0])
+    if row[0] not in ['朝阳市']:
+        city2.append(row[0].replace(u'','市').replace(u'县',''))
 
 ctry = []
 
 pc.execute("""select ctry_name
-	from ctry""")
+    from ctry""")
 for row in pc.fetchall():
-	ctry.append(row[0])
+    ctry.append(row[0])
 
 pc.execute("""select count(1) from ldb1""")
 num, = pc.fetchone()
 pagesize = 10000.0
 for i in range(2): #range(int(ceil(num/pagesize))):
-	pc.execute("""select ctry, 
-		addr, 
-		comp 
-	from ldb1 limit %s offset %s""", (pagesize, pagesize * i))
-	for row in pc.fetchall():
-		proc(row)	
+    pc.execute("""select ctry, 
+        addr, 
+        comp 
+    from ldb1 limit %s offset %s""", (pagesize, pagesize * i))
+    for row in pc.fetchall():
+        proc(row)
---
+++ test1/abc/abc/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  abc
+//
+//  Created by liugehao on 13-11-11.
+//  Copyright (c) 2013年 liugehao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
