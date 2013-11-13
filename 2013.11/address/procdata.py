b3947744a113be131fd911ed92b767d3d9453b84
liugehao <root@workgroup.cn>
2013-11-13 16:41:23 +0800

    up

--- 2013.11/address/procdata.py
+++ 2013.11/address/procdata.py
@@ -6,6 +6,7 @@ import sys
 import re
 reload(sys)
 sys.setdefaultencoding('utf-8')
+from num import getResultForDigit
 
 pconn = psycopg2.connect(host='172.16.147.133', user='postgres', password='l', database='address')
 pconn.set_client_encoding('utf-8')
@@ -21,15 +22,60 @@ def procs(strtmp, a, b=None):
             if strtmp.find(x) == 0:
                 return re.sub(r'^%s' % x, '', strtmp)
     return strtmp
+res['rep0'] = re.compile(u'(对面|门口|旁边|楼上).*$')
+res['rep1'] = re.compile(u'(.*?)\d*号(.*(博物馆|局|[一二三四五]期|分院|学院|大学|嘉园|家园|家属[区楼院]|银行|小区|公司|市场|中心|花园|苑|广场|酒店|大[楼厦院]|公司|学校|分校|宿舍|村屯|村庄|^村镇|^村街|^村路|庄村|^庄镇|^庄街|^庄路|庄屯|屯村|屯庄|^屯镇|村|庄|屯|幼儿园|小学|中学|号院|基地|政府|研究所|产业园|集团)).*$')
+
+res['rep2'] = re.compile(u'(博物馆|局|[一二三四五]期|分院|学院|大学|嘉园|家园|家属[区楼院]|银行|小区|公司|市场|中心|花园|苑|广场|酒店|大[楼厦院]|公司|学校|分校|宿舍|村屯|村庄|^村镇|村街|村路|庄村|^庄镇|庄街|庄路|庄屯|屯村|屯庄|^屯镇|村|庄|屯|幼儿园|小学|中学|号院|基地|政府|研究所|产业园|集团).*$')
+
+res['rep3'] = re.compile(u'(村|庄|屯).*$')
+res['rep4'] = re.compile(u'(街|胡同|弄).*$')
+res['rep5'] = re.compile(u'(路|大道).*$')
+
+res['hao0'] = re.compile(u'(路|街|大道)([一二三四五六七八九][一二三四五六七八九十百千万零]*)')
+res['hao1'] = re.compile(u'(.*?)(\d*)(-|号).*$')
+
+def repnum(tmp1):
+    return res['hao0'].sub(lambda x:x.group(1) + str(getResultForDigit(x.group(2))), tmp1)
+
+def procsa(tmp1):
+    tmp = res['rep1'].sub(lambda x:x.group(1)+x.group(2), tmp1)
+    if tmp != tmp1: return tmp
+    tmp = res['rep2'].sub(lambda x:x.group(1), tmp1)
+    if tmp != tmp1: return tmp
+    tmp = res['rep3'].sub(lambda x:x.group(1), tmp1)
+    if tmp != tmp1: return tmp
+    tmp = res['rep4'].sub(lambda x:x.group(1), tmp1)
+    if tmp != tmp1: return tmp
+    tmp = res['rep5'].sub(lambda x:x.group(1), tmp1)
+    return tmp
+
+def procsb(tmp1):
+    return res['hao1'].sub(lambda x:x.group(1)+'^^^'+x.group(2) , hao0).split('^^^')
+
 
 def proc(row):
-    tmp = procs(row[1].decode('utf-8'), prov1, prov2)
+    tmp2 = row[1].decode('utf-8')
+    tmp = procs(tmp2, prov1, prov2)
     tmp = procs(tmp, city1)
     tmp = procs(tmp, city2)
     tmp = procs(tmp, ctry )
     tmp = procsa(tmp)
-    
-    print row[1],'|', tmp
+    tmp1 = procsb(tmp)
+    print tmp2
+    if len(tmp1) >1:
+        tmp1,tmp2 = tmp1
+        if tmp != tmp1:
+            #tmp1,tmp insert db
+            print '1', tmp
+            print '2', tmp1, tmp2
+            return
+        else:
+            #tmp1 insert db
+            print '2',tmp1
+    else:
+        #tmp insert db
+        print tmp
+    print '-' * 20
     return tmp
 
 
