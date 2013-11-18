50817b17d28aa78eea31e2890e252993e7c0d343
root <root@debian.(none)>
2013-11-18 17:55:27 +0000

    up

--- 2013.11/address/addip.py
+++ 2013.11/address/addip.py
@@ -1,17 +1,28 @@
+#!/usr/bin/env python
+#coding=utf-8
+
 import MySQLdb, psycopg2
 import sys
 import re
 
-res['0'] = re.compile(r'<.*?>')
-res['1'] = re.compile(u'[,.;:，。；：--||]')
+res = {}
+res['0'] = re.compile(u'(<.*?>|&gt;|&lt;|&nbsp;|【.*?】)')
+res['1'] = re.compile(u'[/、◇●,.;:，。；：\-|]')
+res['10'] = re.compile(u'全境派')
 mconn = MySQLdb.connect(charset = 'gbk', host="192.168.1.16", user='caiwu', passwd='cai_Report', db='ydserver')
 mc = mconn.cursor()
 mc.execute("select bm,mc,psfw,bpsfw from gsjj" )
 
 
 def proc(m):
-    tmp = res['0'].sub('', m)
-    tmp = res['1'].sub(' ',tmp)
+    if m is None: return ''
+    try:
+        tmp = res['0'].sub('', m)
+        tmp = res['1'].sub(' ',tmp)
+    except Exception, e:
+        print m
+        print e
+        exit(1)
     return tmp
 
 for m in mc.fetchall():
