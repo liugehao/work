dbd27ab6c09ff6ea305bf4fc4bfd4c76bf230ea8
root@workgroup.cn <root@workgroup.cn>
2013-11-20 19:42:45 +0000

    up

--- 2013.11/address/procdata1.py
+++ 2013.11/address/procdata1.py
@@ -26,6 +26,8 @@ def procs(strtmp, a, b=None):
     return strtmp
 
 res = {}
+res['0'] = re.compile(u'([()·/、,.;:，。；：\-|#]|<.*?>|&.*?;|◇|●|&gt;|&lt;|&nbsp;|【.*?】|　)')
+
 res['rep0'] = re.compile(u'(对面|门口|旁边|楼上).*$')
 res['rep1'] = re.compile(u'(.*?)\d*号(.*(博物馆|局|[一二三四五]期|分院|学院|大学|嘉园|家园|家属[区楼院]|银行|小区|公司|市场|中心|花园|苑|广场|酒店|大[楼厦院]|公司|学校|分校|宿舍|村屯|村庄|^村镇|^村街|^村路|庄村|^庄镇|^庄街|^庄路|庄屯|屯村|屯庄|^屯镇|村|庄|屯|幼儿园|小学|中学|号院|基地|政府|研究所|产业园|集团)).*$')
 
@@ -43,6 +45,7 @@ def repnum(tmp1):
     return res['hao0'].sub(lambda x:x.group(1) + str(getResultForDigit(x.group(2))), tmp1)
 
 def procsa(tmp1):
+    tmp1 = res['0'].sub('',tmp1)
     tmp = res['rep1'].sub(lambda x:x.group(1)+x.group(2), tmp1)
     if tmp != tmp1: return tmp
     tmp = res['rep2'].sub(lambda x:x.group(1), tmp1)
@@ -55,12 +58,13 @@ def procsa(tmp1):
     return tmp
 
 def procsb(tmp1):
+    tmp1 = res['0'].sub('',tmp1)
     tmp = res['hao1'].sub(lambda x:x.group(1)+u'^^^'+x.group(2) , tmp1).split(u'^^^')
     if tmp[0] != tmp1: return tmp
     return res['hao2'].sub(lambda x:x.group(1)+u'^^^'+x.group(2) , tmp1).split(u'^^^')
 
 
-sql_ldb2 = "insert into ldb2 (ctry,addr,comp) values (%s, %s, %s) returning id"
+sql_ldb2 = "insert into psfw (szd,dz,bm,pslb, dzidx) values (%s, %s, %s, 3, to_tsquery('chinesecfg',%s)) "
 
 
 def proc(row):
@@ -71,7 +75,7 @@ def proc(row):
     tmpa = procsa(tmp).replace('-','')
     tmpb = procsb(tmp)
     try:
-        pcw.execute(sql_ldb2, (row[0], tmpa, row[2]))
+        pcw.execute(sql_ldb2, (row[0], tmpa, row[2], tmpa))
     except Exception, e:
         print e
         pconn.rollback()
@@ -82,7 +86,7 @@ def proc(row):
     
     if len(tmpb)== 2 and tmpb[1] != tmpa:
         try:
-            pcw.execute(sql_ldb2, (row[0], tmpb[0], 0))
+            pcw.execute(sql_ldb2, (row[0], tmpb[0], row[2], tmpb[0]))
         except Exception, e:
             print e
             print '2', tmpb
