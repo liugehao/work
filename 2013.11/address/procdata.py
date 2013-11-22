aa35555d7e7b6b9090e0d8d4f8575c4b3055e264
liugehao <root@workgroup.cn>
2013-11-22 10:59:12 +0800

    up

--- 2013.11/address/procdata.py
+++ 2013.11/address/procdata.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #coding=utf-8
 
+from test import repnum, procsa, procsb
 import MySQLdb, psycopg2
 import sys
 import re
@@ -25,39 +26,6 @@ def procs(strtmp, a, b=None):
                 return re.sub(r'^%s' % x, '', strtmp)
     return strtmp
 
-res = {}
-res['rep0'] = re.compile(u'(对面|门口|旁边|楼上).*$')
-res['rep1'] = re.compile(u'(.*?)\d*号(.*(博物馆|局|[一二三四五]期|分院|学院|大学|嘉园|家园|家属[区楼院]|银行|小区|公司|市场|中心|花园|苑|广场|酒店|大[楼厦院]|公司|学校|分校|宿舍|村屯|村庄|^村镇|^村街|^村路|庄村|^庄镇|^庄街|^庄路|庄屯|屯村|屯庄|^屯镇|村|庄|屯|幼儿园|小学|中学|号院|基地|政府|研究所|产业园|集团)).*$')
-
-res['rep2'] = re.compile(u'(博物馆|局|[一二三四五]期|分院|学院|大学|嘉园|家园|家属[区楼院]|银行|小区|公司|市场|中心|花园|苑|广场|酒店|大[楼厦院]|公司|学校|分校|宿舍|村屯|村庄|^村镇|村街|村路|庄村|^庄镇|庄街|庄路|庄屯|屯村|屯庄|^屯镇|村|庄|屯|幼儿园|小学|中学|号院|基地|政府|研究所|产业园|集团).*$')
-
-res['rep3'] = re.compile(u'(村|庄|屯).*$')
-res['rep4'] = re.compile(u'(街|胡同|弄).*$')
-res['rep5'] = re.compile(u'(路|大道).*$')
-
-res['hao0'] = re.compile(u'(路|街|大道)([一二三四五六七八九][一二三四五六七八九十百千万零]*)')
-res['hao1'] = re.compile(u'(.+?)(\d+)(-|号).*$')
-res['hao2'] = re.compile(u'(.+?)(\d+).*$')
-
-def repnum(tmp1):
-    return res['hao0'].sub(lambda x:x.group(1) + str(getResultForDigit(x.group(2))), tmp1)
-
-def procsa(tmp1):
-    tmp = res['rep1'].sub(lambda x:x.group(1)+x.group(2), tmp1)
-    if tmp != tmp1: return tmp
-    tmp = res['rep2'].sub(lambda x:x.group(1), tmp1)
-    if tmp != tmp1: return tmp
-    tmp = res['rep3'].sub(lambda x:x.group(1), tmp1)
-    if tmp != tmp1: return tmp
-    tmp = res['rep4'].sub(lambda x:x.group(1), tmp1)
-    if tmp != tmp1: return tmp
-    tmp = res['rep5'].sub(lambda x:x.group(1), tmp1)
-    return tmp
-
-def procsb(tmp1):
-    tmp = res['hao1'].sub(lambda x:x.group(1)+u'^^^'+x.group(2) , tmp1).split(u'^^^')
-    if tmp[0] != tmp1: return tmp
-    return res['hao2'].sub(lambda x:x.group(1)+u'^^^'+x.group(2) , tmp1).split(u'^^^')
 
 
 sql_ldb2 = "insert into ldb2 (ctry,addr,comp) values (%s, %s, %s) returning id"
